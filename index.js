244154ea7a16a60402161624222ecf08652a365d
Chris Haig <CHAIG200@Caledonian.ac.uk>
2019-07-01 20:36:06 +0100

    commit

--- index.js
+++ index.js
@@ -1,53 +1,42 @@
 
 const data = require('./data/data.json');
 
-var titles = [
-"Project Name",
-"Attachments",
-"Project Description",
-"Supplier Name",
-"Sustainable product/service?",
-"Diverse Supplier",
-"Finance Approver",
-"Finance Comments",
-"Projected Marketing Revenue",
-"D&B Results #1",
-"Committed Spend",
-"Projected Annual Spend",
-"Projected Savings",
-"Projected Revenue",
-"Additional Financial Terms",
-"Geographic Scope",
-"Additional Business Terms",
-"Additional Legal Terms",
-"Effective Date",
-"Contract Expiration Date",
-"Legal Representative",
-"Legal Comments",
-"Legal Action Taken",
-"Notice Address"
-]
+var titles = {
+"Project Name" : {
+    found : false,
+    y: 0
+},
+"Attachments" :{
+    found : false,
+    y: 0
+},
+"Finance Approver" : {
+    found : false,
+    y: 0
+},
+"Finance Comments" : {
+    found : false,
+    y: 0
+},
+"Projected Marketing Revenue" : {
+    found : false,
+    y: 0
+},
+"D&B Results #1" : {
+    found : false,
+    y: 0
+},
+"Workflow Approvals" : {
+    found : false,
+    y: 0
+}
+};
 
-var approvals = [
-    "1.",
-    "2.",
-    "3.",
-    "4.",
-    "5.",
-    "6.",
-    "7.",
-    "8.",
-    "9."
-]
-var top;
+console.log(titles["Project Name"].found)
+
 data["text"].forEach(element => {
-    if(element.text == "Project" || element.text  == "Name" ){
-        top 
-        console.log(element)
+    if(titles["Project Name"].found == false && data["text"].text == "Project"){
+        titles["Project Name"].found = true;
+        titles["Project Name"].y = parseInt(top) - 10;
     }
-   
 });
-
-
-
-
